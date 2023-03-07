3fee096de194c1e32feffd8bc6dfbb36bde400b0
AmandaGxa <AmandaGxa@tfg.co.za>
2023-03-07 17:52:14 +0200

    fiing target part on skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,16 @@
 // import './About.css'
 
-import html from '../img/html.png'
+import styled from 'styled-components'
 import css from '../img/css.png'
+import git from '../img/git.png'
+import html from '../img/html.png'
+import java from '../img/java.png'
 import js from '../img/js.png'
 import node from '../img/node.png'
-import git from '../img/git.png'
-import java from '../img/java.png'
 import react from '../img/react.png'
-import styled from 'styled-components'
 
-function Skills(){
-const Wrapper = styled.section`
+function Skills() {
+    const Wrapper = styled.section`
 
 min-height: 100vh;
 background-color: #F7F7F7;
@@ -18,51 +18,51 @@ display: flex;
 justify-content: center;
 align-items: center;
 `
-const SkillImg = styled.img.attrs((props) => ({
-    src: props.src,
-  }))`
+    const SkillImg = styled.img.attrs((props) => ({
+        src: props.src,
+    }))`
   max-width: 65px;
   border-radius: 5px;
   margin: 10px;
 
   `
-  const Text = styled.p`
+    const Text = styled.p`
 
   line-height: 180%;
   `
- 
 
 
- return (  
- <Wrapper className="container">
-    <div className="row">
-        <div className="col-md-12">
-            <h2>SKILLS</h2>
-        </div>
-        <div className="col-md-1"></div>
-        <div className="col-md-10">
-            <Text>I have an NQF LEVEL 5 in IT (Software Development), and I have experience in HTML, CSS,
-                JavaScript and Java. I use Git version control to host my projects on Github.
-                With these skills I create interative and responsive web experiences. I have some <a href="#projects">projects</a>
-                that I created using all the mentioned skills.
-                I did Java OCA learning, getting to understand Java concepts (primitive, inheritance,
-                polymophism, method and more). I also created some small Java projects which are also on
-                <a href="https://github.com/AmandaGxagxa?tab=repositories&q=&type=&language=java&sort="
-                    target="_blank">Github</a>.
-            </Text>
-            <div className="col-md-1"></div>
-        </div>
-        <div className="col-md-12 ">
-            <SkillImg src={html} alt="HTML Developer" />
-            <SkillImg src={css} alt="CSS Developer" />
-            <SkillImg src={js} alt="JavaScript Developer" />
-            <SkillImg src={node} alt="NodeJS Developer" />
-            <SkillImg src={git} alt="Git Version Control" />
-            <SkillImg src={java} alt="Java Developer" />
-            <SkillImg src={react} alt="React Developer" />
-        </div>
 
-    </div>
-</Wrapper>)
+    return (
+        <Wrapper className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h2>SKILLS</h2>
+                </div>
+                <div className="col-md-1"></div>
+                <div className="col-md-10">
+                    <Text>I have an NQF LEVEL 5 in IT (Software Development), and I have experience in HTML, CSS,
+                        JavaScript and Java. I use Git version control to host my projects on Github.
+                        With these skills I create interative and responsive web experiences. I have some <a href="#projects">projects</a>
+                        that I created using all the mentioned skills.
+                        I did Java OCA learning, getting to understand Java concepts (primitive, inheritance,
+                        polymophism, method and more). I also created some small Java projects which are also on
+                        <a href="https://github.com/AmandaGxagxa?tab=repositories&q=&type=&language=java&sort="
+                            target="_blank" rel="noreferrer" >Github</a>.
+                    </Text>
+                    <div className="col-md-1"></div>
+                </div>
+                <div className="col-md-12 ">
+                    <SkillImg src={html} alt="HTML Developer" />
+                    <SkillImg src={css} alt="CSS Developer" />
+                    <SkillImg src={js} alt="JavaScript Developer" />
+                    <SkillImg src={node} alt="NodeJS Developer" />
+                    <SkillImg src={git} alt="Git Version Control" />
+                    <SkillImg src={java} alt="Java Developer" />
+                    <SkillImg src={react} alt="React Developer" />
+                </div>
+
+            </div>
+        </Wrapper>)
 }
-export default  Skills;
+export default Skills;
